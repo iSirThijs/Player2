01e166d3a1280c5c30745aceb1358f18d211ce26
Thijs Spijker <thijs@tsnetwork.nl>
2019-03-13 10:19:13 +0100

    updated folder structure for views

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const server = express()
+const express = require('express');
+const server = express();
 
 // This data is temporary until there is an api and database connection
-var data = []
+var data = [];
 var newData = [
 	{
 		id: 'ac-odyssey',
@@ -14,7 +14,7 @@ var newData = [
 		title: 'Anno 1800',
 		img: 'https://gpstatic.com/acache/37/76/1/uk/packshot-5646fd847c86d0570aaf2d630505b6ec.jpg alt=cover-anno'
 	}
-]
+];
 
 server
 	.use(express.static('./source/static'))
@@ -25,40 +25,40 @@ server
 	.post('/profile/gamesearch/:id', addGame)
 	.post('/profile/delete/:id', deleteGame)
 	.use(notFound)
-	.listen(8000)
+	.listen(8000);
 
 function notFound(req, res) {
-	res.status(404).send('This page cannot be found')
+	res.status(404).send('This page cannot be found');
 }
 
 function myGamesRender(req, res){
-	res.render('mygames.ejs', {data: data})
+	res.render('profile/game-library.ejs', {data: data});
 }
 
 function gameSearch(req, res) {
-	res.render('gamesearch.ejs', {data: newData})
+	res.render('profile/game-search.ejs', {data: newData});
 }
 
 function addGame(req, res) {
-	let id = req.params.id
+	let id = req.params.id;
 	let game = newData.filter(function (value) {
-		return value.id == id
-	})
-	data.push(game[0])
+		return value.id == id;
+	});
+	data.push(game[0]);
 	newData = newData.filter(function (value) {
-		return value.id !== id
-	})
-	res.status(200).redirect('/profile/games')
+		return value.id !== id;
+	});
+	res.status(200).redirect('/profile/games');
 }
 
 function deleteGame(req, res) {
-	let id = req.params.id
+	let id = req.params.id;
 	let game = data.filter(function (value) {
-		return value.id == id
-	})
-	newData.push(game[0])
+		return value.id == id;
+	});
+	newData.push(game[0]);
 	data = data.filter(function (value) {
-		return value.id !== id
-	})
-	res.status(200).redirect('/profile/games')
+		return value.id !== id;
+	});
+	res.status(200).redirect('/profile/games');
 }
