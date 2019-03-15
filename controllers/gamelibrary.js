58611f1bee0d217b09194587a51b32961947a0fc
Thijs Spijker <thijs@tsnetwork.nl>
2019-03-15 15:13:40 +0100

    seperate data from controllers

--- controllers/gamelibrary.js
+++ controllers/gamelibrary.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const gameSearch = require('./search.js');
+const db = require('../models/data.js');
 
 // This data is temporary until there is an api and database connection
-var data = [
-	{
-		id: 'anno-1800',
-		title: 'Anno 1800',
-		img: 'https://gpstatic.com/acache/37/76/1/uk/packshot-5646fd847c86d0570aaf2d630505b6ec.jpg alt=cover-anno'
-	}
-];
-var newData = [
-	{
-		id: 'ac-odyssey',
-		title: 'Assassin\'s Creed Odyssey',
-		img: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/99/ACOdysseyCoverArt.png/220px-ACOdysseyCoverArt.png alt=cover-ac'
-	}
-];
+var data = [];
 
 router.get('/', libraryRender)
 	.post('/delete/:id', deleteGame)
@@ -29,25 +17,19 @@ function libraryRender(req, res){
 
 function addGame (req, res) {
 	let id = req.params.id;
-	let game = newData.filter(function (value) {
+	let game = db.filter(function (value) {
 		return value.id == id;
 	});
 	data.push(game[0]);
-	newData = newData.filter(function (value) {
-		return value.id !== id;
-	});
 	res.status(200).redirect('/games');
 }
 
 function deleteGame(req, res) {
 	let id = req.params.id;
 	let game = data.filter(function (value) {
-		return value.id == id;
+		return value.id != id;
 	});
-	newData.push(game[0]);
-	data = data.filter(function (value) {
-		return value.id !== id;
-	});
+	data = game;
 	res.status(200).redirect('/games');
 }
 
--- controllers/search.js
+++ controllers/search.js
@@ -1,37 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models/data.js');
 
 // Data to use for the search will be replaced by the API call
-var url
+var url;
 var resultData = [];
-var searchData = [
-	{
-		id: 'ac-odyssey',
-		title: 'Assassin\'s Creed Odyssey',
-		img: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/99/ACOdysseyCoverArt.png/220px-ACOdysseyCoverArt.png alt=cover-ac'
-	}
-];
 
 router.get('/', searchRender);
 router.get('/query?', searchResults);
-router.use(notFound)
+router.use(notFound);
 
 function searchRender(req, res) {
 	res.render('profile/search.ejs', {data: resultData});
 	var baseUrl = req.baseUrl.split('/');
 	baseUrl.pop();
-	url = baseUrl.join('/')
-	console.log(url);
+	url = baseUrl.join('/');
 }
 
 function searchResults(req, res) {
-	resultData.push(searchData[0]);
-	console.log(url)
+	resultData = db;
 	res.redirect(url + '/search');
 }
 
 function notFound(req, res) {
-		res.send('cannot find ' + req.url)
+	res.send('cannot find ' + req.url);
 }
 
 module.exports = router;
